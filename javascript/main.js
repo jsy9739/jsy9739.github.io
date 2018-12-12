2cd49dac43e464134c3871579da3c29a8e91ede1
Jaymart Yabo (RIT Student) <jsy9739@boo-1303-04.main.ad.rit.edu>
2018-12-12 02:24:04 -0500

    edited footer

--- javascript/main.js
+++ javascript/main.js
@@ -2,5 +2,5 @@
 
 //js to update "site last updated" for all pages
 $(document).ready(function(){
-        $("#div1").load("/javascript/date.txt"); //load site last updated text
+     $("#div1").load("/javascript/date.txt"); //load site last updated text
 });
